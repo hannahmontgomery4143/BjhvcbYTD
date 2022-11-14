0f696b925ff61cf8d5d1a441f06eb159eff2d4e8
hannahmontgomery4143 <104352785+hannahmontgomery4143@users.noreply.github.com>
2022-11-14 20:01:40 +0300

    Update repo_linter.sh

--- repo_linter.sh
+++ repo_linter.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Find the repo in the git diff and then set it to an env variables.
-REPO_TO_LINT=$(
 	git diff origin/main -- readme.md |
 	# Look for changes (indicated by lines starting with +).
 	grep ^+ |
-	# Get the line that includes the readme.
 	grep -Eo 'https.*#readme' |
 	# Get just the URL.
 	sed 's/#readme//')
